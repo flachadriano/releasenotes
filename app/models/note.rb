f1c6330df82072fb14b104897186b036e1d3a97a
flachadriano <flachadriano@gmail.com>
2015-06-07 23:50:02 -0300

    type description

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,4 +4,12 @@ class Note < ActiveRecord::Base
 	validates :kind, presence: true
 	validates :component, presence: true
 	validates :description, presence: true, length: {maximum: 140}
+
+	def kind_description
+		case kind
+		when 1 then "New"
+		when 2 then "Change"
+		else "Fix"
+		end
+	end
 end
